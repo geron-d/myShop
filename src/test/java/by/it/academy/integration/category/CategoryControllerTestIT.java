2d4abe9a16fb8e20c2d1e076cff7ee02c109931b
geron_d <geron_d@mail.ru>
2022-07-25 18:18:35 +0300

    h2

--- src/test/java/by/it/academy/integration/category/CategoryControllerTestIT.java
+++ src/test/java/by/it/academy/integration/category/CategoryControllerTestIT.java
@@ -13,17 +13,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @Sql(value = "/scripts/INITIAL_OFFER_TABLE_SCRIPT.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/scripts/AFTER_SCRIPT.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class CategoryControllerTestIT {
 
     @Autowired
@@ -80,7 +85,7 @@ public class CategoryControllerTestIT {
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.content().string(category.getId().toString()))
+                .andExpect(MockMvcResultMatchers.content().string("4"))
                 .andDo(MockMvcResultHandlers.print());
     }
 
